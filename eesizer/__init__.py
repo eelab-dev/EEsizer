8a989a2dcd0b4ac10d11e0a0d5bdd3e26c9024e6
BShark <abshugu@outlook.com>
2025-11-29 21:33:37 +0000

    Revert "1st Stage Pipeline Finished"

--- eesizer/__init__.py
+++
@@ -1,7 +0,0 @@
-"""eesizer package - core refactor scaffold
-
-This package contains modular components for netlist simulation,
-analysis, optimization and orchestration.
-"""
-
-__version__ = "0.0.1"
